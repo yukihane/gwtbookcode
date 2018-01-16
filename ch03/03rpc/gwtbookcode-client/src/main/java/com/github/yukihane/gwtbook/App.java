39cca7ee1516e660267d06d2fd22a3a4fb3dfd39
yukihane <yukihane.feather@gmail.com>
2018-01-16 14:38:54 +0900

    クライアントサイドの送信要求仮実装

--- ch03/03rpc/gwtbookcode-client/src/main/java/com/github/yukihane/gwtbook/App.java
+++ ch03/03rpc/gwtbookcode-client/src/main/java/com/github/yukihane/gwtbook/App.java
@@ -1,16 +1,24 @@
 package com.github.yukihane.gwtbook;
 
+import com.github.yukihane.gwtbook.entity.CalcRequestData;
+import com.github.yukihane.gwtbook.entity.CalcResult;
 import com.github.yukihane.gwtbook.entity.Room;
 import com.github.yukihane.gwtbook.entity.Student;
+import com.github.yukihane.gwtbook.service.CalcService;
+import com.github.yukihane.gwtbook.service.CalcServiceAsync;
 import com.github.yukihane.gwtbook.widget.StudentTable;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.view.client.ListDataProvider;
 
 public class App implements EntryPoint {
+
+    private final CalcServiceAsync calcService = GWT.create(CalcService.class);
 
     @Override
     public void onModuleLoad() {
@@ -31,9 +39,32 @@ public class App implements EntryPoint {
             GWT.log("追加ボタンが押されました size: " + studentProvider.getList().size());
         });
 
+        final Button sendButton = new Button("送信");
+        sendButton.addClickHandler(event -> {
+            final CalcRequestData input = null;
+
+            calcService.calculate(input, new AsyncCallback<CalcResult>() {
+
+                @Override
+                public void onSuccess(final CalcResult result) {
+                    GWT.log("onSuccess: " + result);
+                }
+
+                @Override
+                public void onFailure(final Throwable caught) {
+                    GWT.log("onFailure");
+                }
+            });
+        });
+
         final VerticalPanel vpanel = new VerticalPanel();
         vpanel.add(cellTable);
-        vpanel.add(addButton);
+
+        final FlowPanel fpanel = new FlowPanel();
+        fpanel.add(addButton);
+        fpanel.add(sendButton);
+
+        vpanel.add(fpanel);
 
         RootPanel.get().add(vpanel);
 
