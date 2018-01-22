a7f93578328d21350d14915c657939a7361da67e
yukihane <yukihane.feather@gmail.com>
2018-01-22 19:03:52 +0900

    01minimalをコピー

---
+++ ch04/02frontend/gwtbookcode-client/src/main/java/com/github/yukihane/gwtbook/App.java
@@ -0,0 +1,12 @@
+package com.github.yukihane.gwtbook;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+public class App implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        GWT.log("Hello, GWT!");
+    }
+}
